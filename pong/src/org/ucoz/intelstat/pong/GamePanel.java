779c3bc18428b5dc60f1310367b7e3aa6c3066c1
InteLStaT <InteLStaT@users.noreply.github.com>
2018-07-18 20:00:48 +0300

    Integrated GameStage features

--- pong/src/org/ucoz/intelstat/pong/GamePanel.java
+++ pong/src/org/ucoz/intelstat/pong/GamePanel.java
@@ -5,6 +5,7 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import java.util.concurrent.TimeUnit;
 
 import javax.swing.JPanel;
 
@@ -14,20 +15,24 @@ public class GamePanel extends JPanel implements Runnable {
 	public static final int MIN_IUFPS = 8, MIN_IRFPS = 8;
 	
 	public final int WIDTH, HEIGHT;
-	public final int IUFPS, IRFPS;
+	public final int IUFPMS, IRFPS; // read: inverse of update/render frames per millisecond (update/render milliseconds per frame)
 	private boolean started = false;
 	private int time;
 	
 	private final BufferedImage osc;
 	private final Graphics2D g;
 	private final Color BG_COLOR = Color.BLACK;
+	
+	private GameStage currentStage;
 
-	private Thread gameLoop = new Thread(this);
+	private Thread gameLoop;
+	private long lastRealNanoTime;
+	private long timeToSleep;
 
 	public GamePanel(int width, int height, int iufps, int irfps) {
 		WIDTH = width < MIN_WIDTH ? MIN_WIDTH : width;
 		HEIGHT = width < MIN_HEIGHT ? MIN_HEIGHT : height;
-		IUFPS = iufps < MIN_IUFPS ? MIN_IUFPS : iufps;
+		IUFPMS = iufps < MIN_IUFPS ? MIN_IUFPS : iufps;
 		IRFPS = irfps < MIN_IRFPS ? MIN_IRFPS : irfps;
 		
 		osc = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
@@ -39,25 +44,62 @@ public class GamePanel extends JPanel implements Runnable {
 		requestFocus();
 	}
 	
-	@Override
-	public void paintComponent(Graphics gp) {
-		g.clearRect(0, 0, WIDTH, HEIGHT);
-		
-		// TODO: fill in with stage gfx
-		gp.drawImage(osc, 0, 0, null);
-	}
-
 	public void start() {
 		if(started) {
 			throw new IllegalStateException("game already started");
 		}
+		started = true;
+		gameLoop = new Thread(this);
 		gameLoop.start();
+	}
+	
+	public void stop() {
+		started = false;
+	}
+	
+	/**
+	 * Warning: not thread-safe
+	 */
+	public void setStage(GameStage newStage) {
+		currentStage = newStage;
+	}
+	
+	@Override
+	public void paintComponent(Graphics gp) {
+		g.clearRect(0, 0, WIDTH, HEIGHT);
+		currentStage.renderFrame(time, IRFPS, g);
+		// TODO: fill in with stage gfx
+		gp.drawImage(osc, 0, 0, null);
 	}
 	
 	@Override
 	public void run() {
-		while (true) {
-			time += IUFPS;
+		long IUFPNS = IUFPMS * 1_000_000; // read: inverse of update frames per nanosecond (update nanoseconds per frame)
+		boolean advanceStage;
+		while (started) {
+			lastRealNanoTime = System.nanoTime();
+			time += IUFPMS;
+			
+			advanceStage = currentStage.update(time, IUFPMS);
+			repaint(); // TODO: this is to be moved to a render loop
+			if(advanceStage) {
+				GameStage nextStage = currentStage.nextStage();
+				if(nextStage != null) {
+					currentStage = nextStage;
+				}
+			}
+			
+			
+			try {
+				timeToSleep = IUFPNS - (System.nanoTime() - lastRealNanoTime);
+				if(timeToSleep < 0) {
+					timeToSleep = 0;
+				}
+				
+				TimeUnit.NANOSECONDS.sleep(timeToSleep);
+			} catch(InterruptedException e) {
+				
+			}
 		}
 	}
 
