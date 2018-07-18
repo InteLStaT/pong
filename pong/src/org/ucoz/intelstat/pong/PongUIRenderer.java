ee83cb1ca8e6d2e74f000e0b9fe4f56524709512
InteLStaT <InteLStaT@users.noreply.github.com>
2018-07-18 21:19:05 +0300

    Added PongUIRenderer default implementation

---
+++ pong/src/org/ucoz/intelstat/pong/PongUIRenderer.java
@@ -0,0 +1,39 @@
+package org.ucoz.intelstat.pong;
+
+import java.awt.BasicStroke;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics2D;
+import java.awt.Stroke;
+import java.util.HashSet;
+
+import org.ucoz.intelstat.pong.UIControlHandler.Button;
+import org.ucoz.intelstat.pong.UIControlHandler.Control;
+import org.ucoz.intelstat.pong.UIControlHandler.Panel;
+
+public class PongUIRenderer implements UIControlHandler.Renderer {
+
+	private Font defaultFont = new Font("Century Gothic", Font.BOLD, 32);
+	private Stroke buttonStroke = new BasicStroke(5);
+
+	@Override
+	public void render(Graphics2D g, HashSet<Control> controls) {
+		render_impl(g, controls, 0, 0);
+	}
+
+	private void render_impl(Graphics2D g, HashSet<Control> controls, int offx, int offy) {
+		for (Control control : controls) {
+			if (control instanceof Panel) {
+				render_impl(g, ((Panel) control).children(), control.posx, control.posy);
+			} else if (control instanceof Button) {
+				Button button = (Button) control;
+				g.setStroke(buttonStroke);
+				g.drawRect(offx + button.posx, offy + button.posy, button.width, button.height);
+				g.setFont(defaultFont);
+				FontMetrics fm = g.getFontMetrics();
+				g.drawString(button.text(), offx + button.posx + (button.width - fm.stringWidth(button.text())) / 2,
+						offy + button.posy + (button.height - fm.getAscent()) / 2);
+			}
+		}
+	}
+}
