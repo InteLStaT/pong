ae5482b705f1810cf4b10796480160b27806dd22
InteLStaT <InteLStaT@users.noreply.github.com>
2018-07-19 23:32:36 +0300

    Rewrote for new UIControlHandler

--- pong/src/org/ucoz/intelstat/pong/MainMenuStage.java
+++ pong/src/org/ucoz/intelstat/pong/MainMenuStage.java
@@ -1,16 +1,27 @@
 package org.ucoz.intelstat.pong;
 
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics2D;
+import java.awt.Toolkit;
 import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
-import org.ucoz.intelstat.pong.UIControlHandler.Button;
-import org.ucoz.intelstat.pong.UIControlHandler.Panel;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JPanel;
 
 public class MainMenuStage extends GameStage {
 
-	private UIControlHandler uich = new UIControlHandler();
+	private UIControlHandler uich;
 	private UIControlHandler.Renderer uir = new PongUIRenderer();
 	private GameStage nextStage = null;
+	
+	private int width, height;
+	
+	private Font titleFont = new Font("Century Gothic", Font.BOLD, 72);
+	private FontMetrics titleFontMetrics = Toolkit.getDefaultToolkit().getFontMetrics(titleFont);
 	
 	public MainMenuStage() {
 		super("main-menu");
@@ -18,19 +29,34 @@ public class MainMenuStage extends GameStage {
 	
 	@Override
 	public void onLoad(GameStage prevStage, int width, int height) {
-		Panel panel = new Panel(width/3, height/3, width/3, height*2/3);
-		Button btnNewGame = new Button(0, 0, width/3, 150, "New Game");
-		btnNewGame.setAction((e) -> {
-			// TODO: set nextStage to a new game stage thing
+		this.width = width;
+		this.height = height;
+		
+		uich = new UIControlHandler(width, height);
+		
+		JPanel panel = new JPanel();
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+		panel.setLocation(width/3, height*2/5);
+		panel.setSize(new Dimension(width/3, height*3/5));
+		
+		JButton btnNewGame = new JButton("New game");
+		//btnNewGame.setLocation(0, 0);
+		btnNewGame.setSize(new Dimension(width/3, 100));
+		btnNewGame.addActionListener((e) -> {
 			nextStage = new MatchStage();
 		});
-		panel.addControl(btnNewGame);
+		panel.add(btnNewGame);
+		
 		uich.addControl(panel);
 	}
 	
 	@Override
 	public void renderFrame(int time, int irfps, Graphics2D g) {
-		uir.render(g, uich.controls());
+		String title = "Pong";
+		g.setFont(titleFont);
+		g.drawString(title, (width - titleFontMetrics.stringWidth(title)) / 2, height/5);
+		
+		uir.render(g, uich);
 	}
 
 	@Override
@@ -48,6 +74,11 @@ public class MainMenuStage extends GameStage {
 
 	@Override
 	public MouseListener mouseListener() {
+		return uich.mouseListener();
+	}
+	
+	@Override
+	public MouseMotionListener mouseMotionListener() {
 		return uich.mouseListener();
 	}
 	
