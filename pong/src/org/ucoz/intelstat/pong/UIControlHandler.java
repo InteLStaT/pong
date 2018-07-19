aba8366991d19ced3040b5e6ec0f06742d99edd2
InteLStaT <InteLStaT@users.noreply.github.com>
2018-07-19 23:31:33 +0300

    Rewrote entirely, now uses Swing components, added default Renderer impl

--- pong/src/org/ucoz/intelstat/pong/UIControlHandler.java
+++ pong/src/org/ucoz/intelstat/pong/UIControlHandler.java
@@ -1,157 +1,127 @@
 package org.ucoz.intelstat.pong;
 
+import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.Graphics2D;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.util.HashSet;
+
+import javax.swing.AbstractButton;
+import javax.swing.ButtonModel;
+import javax.swing.JComponent;
+import javax.swing.JPanel;
 
 public class UIControlHandler {
-
-	private HashSet<Control> controls = new HashSet<>();
 	
-	public UIControlHandler() {
-		
+	private JPanel container = new JPanel(null);
+	
+	public UIControlHandler(int width, int height) {
+		container.setSize(new Dimension(width, height));
 	}
 	
-	public void addControl(Control control) {
-		controls.add(control);
+	public void addControl(JComponent control) {
+		container.add(control);
 	}
 	
-	public void removeControl(Control control) {
-		controls.remove(control);
+	public void removeControl(JComponent control) {
+		container.remove(control);
 	}
 	
-	public HashSet<Control> controls() {
-		return controls;
+	public Component[] controls() {
+		return container.getComponents();
 	}
 	
-	public MouseListener mouseListener() {
-		return new MouseListener() {
+	public MouseAdapter mouseListener() {
+		return new MouseAdapter() {
 
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				HashSet<Control> currentControls = controls;
-				boolean found = false;
-				while(!found) {
-					for(Control control : currentControls) {
-						if(control instanceof Panel && control.isInBounds(e.getX(), e.getY())) {
-							currentControls = ((Panel) control).children;
-							continue;
-						} else if(control instanceof Button) {
-							Button button = (Button) control;
-							if(button.action != null) {
-								button.action.actionPerformed(new ActionEvent(button, 0, ""));
-								found = true;
-								break;
-							}
-						} else {
-							found = true;							
-						}
-					}
-				}
-				
+				Component control = container.findComponentAt(e.getPoint());
+				e.setSource(control);
+				control.dispatchEvent(e);
 			}
 
 			@Override
 			public void mousePressed(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
+				Component control = container.findComponentAt(e.getPoint());
+				e.setSource(control);
+				control.dispatchEvent(e);
+				if(control instanceof AbstractButton) {
+					AbstractButton button = (AbstractButton) control;
+					ButtonModel model = button.getModel();
+					model.setArmed(true);
+					model.setPressed(true);
+				}
 			}
 
 			@Override
 			public void mouseReleased(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
+				Component control = container.findComponentAt(e.getPoint());
+				e.setSource(control);
+				control.dispatchEvent(e);
+				if(control instanceof AbstractButton) {
+					AbstractButton button = (AbstractButton) control;
+					ButtonModel model = button.getModel();
+					model.setPressed(false);
+					model.setArmed(false);
+				}
 			}
 
 			@Override
 			public void mouseEntered(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
+
 			}
 
 			@Override
 			public void mouseExited(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
+
 			}
 			
+			@Override
+			public void mouseMoved(MouseEvent e) {
+				//Component control = container.findComponentAt(e.getPoint());
+				//control.dispatchEvent(e);
+			}
+			
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				//Component control = container.findComponentAt(e.getPoint());
+				//control.dispatchEvent(e);
+			}
 		};
 	}
 	
-	public static abstract class Control {
-		int posx, posy;
-		int width, height;
-		Control parent;
-
-		public Control(int posx, int posy, int width, int height) {
-			this.posx = posx;
-			this.posy = posy;
-			this.width = width;
-			this.height = height;
-		}
-
-		public final boolean isInBounds(int x, int y) {
-			return x > posx && y > posy && x < posx + width && y < posy + height;
-		}
-
-	}
-
-	public static class Button extends Control {
-
-		private ActionListener action;
-		private String text;
-		
-		public Button(int posx, int posy, int width, int height, String text) {
-			super(posx, posy, width, height);
-			this.text = text;
-		}
-
-		public void setAction(ActionListener action) {
-			this.action = action;
-		}
-		
-		public String text() {
-			return text;
-		}
-		
-		public void text(String text) {
-			this.text = text;
-		}
-		
-	}
-
-	public static class Panel extends Control {
-
-		private HashSet<Control> children = new HashSet<>();
-
-		public Panel(int posx, int posy, int width, int height) {
-			super(posx, posy, width, height);
-		}
-
-		public void addControl(Control control) {
-			if (control.parent instanceof Panel) {
-				((Panel) control.parent).removeControl(control);
+	public KeyListener keyListener() {
+		return new KeyListener() {
+			
+			@Override
+			public void keyTyped(KeyEvent e) {
+				container.dispatchEvent(e);
 			}
-			control.parent = this;
-			children.add(control);
-		}
-
-		public void removeControl(Control control) {
-			children.remove(control);
-			control.parent = null;
-		}
-		
-		public HashSet<Control> children() {
-			return children;
-		}
+			
+			@Override
+			public void keyReleased(KeyEvent e) {
+				container.dispatchEvent(e);
+			}
+			
+			@Override
+			public void keyPressed(KeyEvent e) {
+				container.dispatchEvent(e);
+			}
+		};
 	}
 	
 	public static interface Renderer {
 		
-		public void render(Graphics2D g, HashSet<Control> controls);
+		public default void render(Graphics2D g, UIControlHandler uich) {
+			for(Component control : uich.controls()) {
+				g.translate(control.getX(), control.getY());
+				control.print(g);
+				g.translate(-control.getX(), -control.getY());
+			}
+		}
 		
 	}
 
