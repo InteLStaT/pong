43135927d4cae40349703b3b493068ba5745bf24
InteLStaT <InteLStaT@users.noreply.github.com>
2018-07-19 23:31:55 +0300

    Rewrote for new UIControlHandler

--- pong/src/org/ucoz/intelstat/pong/PongUIRenderer.java
+++ pong/src/org/ucoz/intelstat/pong/PongUIRenderer.java
@@ -1,40 +1,40 @@
 package org.ucoz.intelstat.pong;
 
 import java.awt.BasicStroke;
+import java.awt.Component;
 import java.awt.Font;
 import java.awt.FontMetrics;
 import java.awt.Graphics2D;
 import java.awt.Stroke;
 import java.awt.Toolkit;
-import java.util.HashSet;
 
-import org.ucoz.intelstat.pong.UIControlHandler.Button;
-import org.ucoz.intelstat.pong.UIControlHandler.Control;
-import org.ucoz.intelstat.pong.UIControlHandler.Panel;
+import javax.swing.JButton;
+import javax.swing.JPanel;
 
 public class PongUIRenderer implements UIControlHandler.Renderer {
 
-	private Font defaultFont = new Font("Century Gothic", Font.BOLD, 32);
-	private FontMetrics defaultFontMetrics = Toolkit.getDefaultToolkit().getFontMetrics(defaultFont);
+	private Font buttonBigFont = new Font("Century Gothic", Font.BOLD, 50);
+	private FontMetrics buttonBigFontMetrics = Toolkit.getDefaultToolkit().getFontMetrics(buttonBigFont);
 	private Stroke buttonStroke = new BasicStroke(5);
 
 	@Override
-	public void render(Graphics2D g, HashSet<Control> controls) {
-		render_impl(g, controls, 0, 0);
+	public void render(Graphics2D g, UIControlHandler uich) {
+		render_impl(g, uich.controls(), 0, 0);
 	}
 
-	private void render_impl(Graphics2D g, HashSet<Control> controls, int offx, int offy) {
-		for (Control control : controls) {
-			if (control instanceof Panel) {
-				render_impl(g, ((Panel) control).children(), control.posx, control.posy);
-			} else if (control instanceof Button) {
-				Button button = (Button) control;
+	private void render_impl(Graphics2D g, Component[] controls, int offx, int offy) {
+		for (Component control : controls) {
+			if (control instanceof JButton) {
+				JButton button = (JButton) control;
 				g.setStroke(buttonStroke);
-				g.drawRect(offx + button.posx, offy + button.posy, button.width, button.height);
-				g.setFont(defaultFont);
-				g.drawString(button.text(),
-						offx + button.posx + (button.width - defaultFontMetrics.stringWidth(button.text())) / 2,
-						offy + button.posy + (button.height + defaultFontMetrics.getAscent()) / 2);
+				g.drawRect(offx + button.getX(), offy + button.getY(), button.getWidth(), button.getHeight());
+				g.setFont(buttonBigFont);
+				g.drawString(button.getText(),
+						offx + button.getX() + (button.getWidth() - buttonBigFontMetrics.stringWidth(button.getText())) / 2,
+						offy + button.getY() + (button.getHeight() + buttonBigFontMetrics.getAscent()) / 2 - 5);
+
+			} else if (control instanceof JPanel) {
+				render_impl(g, ((JPanel) control).getComponents(), control.getX(), control.getY());
 			}
 		}
 	}
